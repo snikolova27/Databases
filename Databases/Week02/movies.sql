3afa79847023e91abc57987df65027dd316542f9
Sonya Nikolova <snikolova27@gmail.com>
2023-03-19 14:22:17 +0200

    add week02 movies tasks

---
+++ Databases/Week02/movies.sql
@@ -0,0 +1,18 @@
+use movies;
+-- Напишете заявка, която извежда имената на актрисите, участвали в Terms of
+-- Endearment
+
+SELECT NAME from MOVIESTAR
+JOIN STARSIN ON name = STARNAME
+JOIN MOVIE on MOVIETITLE = TITLE
+WHERE MOVIETITLE = 'Terms of Endearment'
+AND GENDER = 'F';
+
+--  Напишете заявка, която извежда имената на филмовите звезди,  участвали във филми на студио MGM през
+-- 1995 г.
+
+SELECT MOVIESTAR.NAME FROM MOVIESTAR
+JOIN STARSIN ON name = STARNAME
+JOIN MOVIE ON TITLE = MOVIETITLE
+JOIN STUDIO ON Studio.NAME = STUDIONAME
+WHERE MOVIEYEAR = '1995';
